080ea1523350e370c56bb3f49aadf4dca9c2e31d
swapnil-naarwade <swapnilnarwade123@gmail.com>
2018-03-18 17:58:38 -0500

    This is my first assignment in the udemy course

--- practice/src/app/app.module.ts
+++ practice/src/app/app.module.ts
@@ -3,14 +3,14 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlert } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent
+    WarningAlertComponent,
+    SuccessAlert
   ],
   imports: [
     BrowserModule
