613a507e5f9792c151c7a8b7ca7c0137cbcfb71e
swapnil-naarwade <swapnilnarwade123@gmail.com>
2018-03-19 19:12:57 -0500

    till i come back from NSBE

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowUser = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    setTimeout(() => {
+      this.allowUser = true;
+    }, 2000);
   }
+  paragraph = "no server located ";
+  onClickRun(){
+    this.paragraph = "server located";
+  }
+
+  website : string = "www.swapnilnarwade@mavs.uta.edu" ;
+  name : string = "swapnil";
+  lastname : string = "narwade";
+  idNuber : number = 100125;
+
+getname(){
+  return this.name;
+}
+ngOnInit() {
+}
 
 }
